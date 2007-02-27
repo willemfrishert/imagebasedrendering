e9e2868e806aa3cade3c5aeae24f1b760d80bdb3
jpjorge <jpjorge@15324175-3028-0410-9899-2d1205849c9d>
2007-02-27 19:30:24 +0000

    using vertex shader; copying the original texture; receiving as an argument the internal format and the format of the values

--- trunk/Project/Code/src/FloatTextProcessor.cpp
+++ trunk/Project/Code/src/FloatTextProcessor.cpp
@@ -5,7 +5,7 @@
 #include "ShaderProgram.h"
 #include "FloatTextProcessor.h"
 
-void check(char* str)
+inline void check(char* str)
 {
 	static GLenum errCode;
 	const GLubyte *errString;
@@ -18,19 +18,81 @@ void check(char* str)
 }
 
 
-FloatTextProcessor::FloatTextProcessor( float* values, int width, int height )
-: iValues( values )
-, iWidth( width )
+FloatTextProcessor::FloatTextProcessor( float* values, int width, int height, string fragmentShaderFilename, 
+									   GLint texInternalFormat, GLenum texFormat )
+: iWidth( width )
 , iHeight( height )
+, internalFormat( texInternalFormat )
+, format( texFormat )
 {
 	iFbo = new FrameBufferObject();
 
+	// generate the necessary textures: 3 (1 to store the original + 2 for ping-pong)	
+	generateTextures(values);
+
+	// Shader misc.
+	initShaders( fragmentShaderFilename );
+	
+}
+
+FloatTextProcessor::FloatTextProcessor(GLuint originalTexId, int width, int height, 
+									   string fragmentShaderFilename, GLint texInternalFormat, GLenum texFormat)
+: iWidth( width )
+, iHeight( height )
+, iOriginalTexture( originalTexId )
+, internalFormat( texInternalFormat )
+, format( texFormat )
+{
+	iFbo = new FrameBufferObject();
+
+	// generate the necessary textures: 3 (1 to store the original + 2 for ping-pong)	
+	generateTextures(NULL);
+
+	// Shader misc.
+	initShaders( fragmentShaderFilename );
+}
+
+void FloatTextProcessor::initShaders( string fragmentShaderFilename )
+{
+	iShaderProgram			= new ShaderProgram();
+	iInputTextureUniform	= new ShaderUniformValue<int>();
+
+	iFragmentShader			= new ShaderObject(GL_FRAGMENT_SHADER, fragmentShaderFilename);
+	
+	// hardcoded: doesn't change
+	iVertexShader			= new ShaderObject(GL_VERTEX_SHADER, "./shader/computecoords.vert");
+	
+	iInputTextureUniform->setValue( 0 );
+	iInputTextureUniform->setName("inputTexture");
+
+	iShaderProgram->attachShader( *iFragmentShader );
+	iShaderProgram->attachShader( *iVertexShader );
+	iShaderProgram->addUniformObject( iInputTextureUniform );
+
+	// after all the shaders have been attached
+	iShaderProgram->buildProgram();
+}
+
+void FloatTextProcessor::generateTextures( float* originalData )
+{
 	// generate texture id's
 	glGenTextures(2, iTextures);
 
+	setupTexture(iTextures[0], 0);
+	setupTexture(iTextures[1], 0);
+
+	if ( originalData != NULL)
+	{
+		glGenTextures(1, &iOriginalTexture);
+		setupTexture(iOriginalTexture, originalData);
+	}
+}
+
+void FloatTextProcessor::setupTexture(GLuint textureId, const GLvoid* data)
+{
 	// NOTE: use GL_TEXTURE_RECTANGLE_ARB in order to specify coords
 	// from [0, height] x [0, width]
-	glBindTexture(GL_TEXTURE_RECTANGLE_ARB, iTextures[0]);
+	glBindTexture(GL_TEXTURE_RECTANGLE_ARB, textureId);
 
 	glTexParameteri(GL_TEXTURE_RECTANGLE_ARB, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_RECTANGLE_ARB, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
@@ -43,40 +105,23 @@ FloatTextProcessor::FloatTextProcessor( float* values, int width, int height )
 
 	glTexImage2D(GL_TEXTURE_RECTANGLE_ARB,  // Need to use texture rectangle to get GPU-side floats
 		0,						// Mipmap level 0
-		GL_RGB_FLOAT32_ATI,		// Store on GPU as 3-component float (internalFormat)
+		internalFormat,			// Store on GPU as 3-component float (internalFormat)
 		iWidth,					// width
 		iHeight,				// height
 		0,						// border
-		GL_LUMINANCE,			// Source data is 1-component
+		format,					// Source data is 1-component
 		GL_FLOAT,				// Source data is floating point numbers
-		iValues);				// Texture data
-
-	glBindTexture(GL_TEXTURE_RECTANGLE_ARB, iTextures[1]);
-	glTexParameteri(GL_TEXTURE_RECTANGLE_ARB, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_RECTANGLE_ARB, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_RECTANGLE_ARB, GL_TEXTURE_WRAP_S, GL_CLAMP);
-	glTexParameteri(GL_TEXTURE_RECTANGLE_ARB, GL_TEXTURE_WRAP_T, GL_CLAMP);
-
-	// The iWidth/2 x iHeight/2 is enough for the rest of the generated textures
-	glTexImage2D(GL_TEXTURE_RECTANGLE_ARB, 0, GL_RGB_FLOAT32_ATI, 
-		iWidth, iHeight, 0, GL_LUMINANCE, GL_FLOAT, 0);
-
-	// Shader misc.
-	iShaderProgram			= new ShaderProgram();
-	iInputTextureUniform	= new ShaderUniformValue<int>();
-	iFragmentShader			= new ShaderObject(GL_FRAGMENT_SHADER, "./shader/max.frag");
-	iInputTextureUniform->setValue( 0 );
-	iInputTextureUniform->setName("inputTexture");
-
-	iShaderProgram->attachShader( *iFragmentShader );
-	iShaderProgram->addUniformObject( iInputTextureUniform );
-
-	// after all the shaders have been attached
-	iShaderProgram->buildProgram();
+		data);					// Texture data
 }
 
-void FloatTextProcessor::processData()
+/**
+ * @description Processes the Parallel Reduction algorithm given the passed shader
+ * @return the result of the algorithm
+ */
+float FloatTextProcessor::processData()
 {
+	glViewport(0, 0, iWidth, iHeight);
+
 	int curReadTex = 0;
 	int curWriteTex = 1;
 
@@ -94,19 +139,22 @@ void FloatTextProcessor::processData()
 
 	iShaderProgram->useProgram();
 
-	for (int i = 0; i < 3/*targetWidth > 1*/; i++)
+	// First, do the first pass of the reduction from the original to one of the 
+	// ping-pong textures so we can continue using them without corrupting the original
+	iFbo->attachTexture(iTextures[ 0 ], GL_COLOR_ATTACHMENT0_EXT, 
+		GL_TEXTURE_RECTANGLE_ARB, 0);
+
+	// and finally, draw, and do the computations on the fragment shader
+	renderSceneOnQuad(iOriginalTexture, GL_TEXTURE_RECTANGLE_ARB, 
+		targetWidth, targetHeight);
+
+	for (int i = 0; targetWidth > 1; i++)
 	{
 		curReadTex	= i % 2;
 		curWriteTex = (i+1) % 2;
-	
-		// Don't forget to clean the COLOR/DEPTH buffers
-		//glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-		//glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		// define the viewport to be 1/4 of the previous texture
-		// the fragment shader will output 1/4 of the data
-		//glViewport(0, 0, targetWidth, targetHeight);
-		glViewport(0, 0, iWidth, iHeight);
+		targetWidth		/= 2;
+		targetHeight	/= 2;
 
 		// attach the current write texture: it should be the original one,
 		// or the previously written one
@@ -116,23 +164,20 @@ void FloatTextProcessor::processData()
 		// and finally, draw, and do the computations on the fragment shader
 		renderSceneOnQuad(iTextures[curReadTex], GL_TEXTURE_RECTANGLE_ARB, 
 			targetWidth, targetHeight);
-
-		printf("Reading from texture id: %d \n", iTextures[ curReadTex ]);
-		printf("Writing to texture id: %d \n", iTextures[ curWriteTex ]);
-
-		targetWidth		/= 2;
-		targetHeight	/= 2;
 	}
 
 	iShaderProgram->disableProgram();
 
 	const int side = 1;
-	GLfloat retVal[side*side*4];
+	GLfloat result[side*side*4];
 	glReadBuffer(GL_COLOR_ATTACHMENT0_EXT);
-	glReadPixels(0, 0, side, side, GL_RGBA, GL_FLOAT, &retVal);
+	glReadPixels(0, 0, side, side, GL_RGBA, GL_FLOAT, &result);
 
 	// we can draw back to screen again, if we want :)
 	iFbo->unbind();
+
+	// any component will do: (V, V, V, 1)
+	return result[ 0 ];
 }
 
 void FloatTextProcessor::renderSceneOnQuad(GLuint textureId, 
@@ -146,80 +191,16 @@ void FloatTextProcessor::renderSceneOnQuad(GLuint textureId,
 			glMatrixMode( GL_PROJECTION );
 			glPushMatrix();
 			glLoadIdentity();
-			//gluOrtho2D( -1.0f, 1.0f, -1.0f, 1.0f );
 			gluOrtho2D( 0.0f, iWidth, 0.0f, iHeight);
-			//gluOrtho2D( 0.0f, iWidth, 0.0f, iHeight );
 		
 			glMatrixMode( GL_MODELVIEW );
 			glPushMatrix();
 			glLoadIdentity();
-			glEnable( target );
 			
 	}
 
-	//glActiveTexture(GL_TEXTURE0);
-	//glBindTexture(target, textureId);
-	//glActiveTexture(GL_TEXTURE1);
-	//glBindTexture(target, textureId);
-	//glActiveTexture(GL_TEXTURE2);
-	//glBindTexture(target, textureId);
-	//glActiveTexture(GL_TEXTURE3);
-	//glBindTexture(target, textureId);
-
 	glBindTexture(target, textureId);
 	glBegin(GL_QUADS);
-	//glMultiTexCoord2f(GL_TEXTURE0, -0.5, -0.5);
-	//glMultiTexCoord2f(GL_TEXTURE1,  0.5, -0.5);
-	//glMultiTexCoord2f(GL_TEXTURE2, -0.5,  0.5);
-	//glMultiTexCoord2f(GL_TEXTURE3,  0.5,  0.5);
-	////glVertex2f( -1.0f, -1.0f );
-	//glVertex2f( 0, 0 );
-
-	//glMultiTexCoord2f(GL_TEXTURE0, 2*static_cast<GLfloat>(width)-0.5, -0.5);
-	//glMultiTexCoord2f(GL_TEXTURE1, 2*static_cast<GLfloat>(width)+0.5, -0.5);
-	//glMultiTexCoord2f(GL_TEXTURE2, 2*static_cast<GLfloat>(width)-0.5,  0.5);
-	//glMultiTexCoord2f(GL_TEXTURE3, 2*static_cast<GLfloat>(width)+0.5,  0.5);
-	////glVertex2f( 1.0f, -1.0f );
-	//glVertex2f( width, 0 );
-
-	//glMultiTexCoord2f(GL_TEXTURE0, 2*static_cast<GLfloat>(width)-0.5, 
-	//	2*static_cast<GLfloat>(height)-0.5);
-	//glMultiTexCoord2f(GL_TEXTURE1, 2*static_cast<GLfloat>(width)+0.5, 
-	//	2*static_cast<GLfloat>(height)-0.5);
-	//glMultiTexCoord2f(GL_TEXTURE2, 2*static_cast<GLfloat>(width)-0.5, 
-	//	2*static_cast<GLfloat>(height)+0.5);
-	//glMultiTexCoord2f(GL_TEXTURE3, 2*static_cast<GLfloat>(width)+0.5, 
-	//	2*static_cast<GLfloat>(height)+0.5);
-	////glVertex2f( 1.0f, 1.0f );
-	//glVertex2f( width, height );
-
-	//glMultiTexCoord2f(GL_TEXTURE0, 2*static_cast<GLfloat>(width)-0.5, 
-	//	2*static_cast<GLfloat>(height)-0.5);
-	//glMultiTexCoord2f(GL_TEXTURE1, 2*static_cast<GLfloat>(width)+0.5, 
-	//	2*static_cast<GLfloat>(height)-0.5);
-	//glMultiTexCoord2f(GL_TEXTURE2, 2*static_cast<GLfloat>(width)-0.5, 
-	//	2*static_cast<GLfloat>(height)+0.5);
-	//glMultiTexCoord2f(GL_TEXTURE3, 2*static_cast<GLfloat>(width)+0.5, 
-	//	2*static_cast<GLfloat>(height)+0.5);
-	////glVertex2f( -1.0f, 1.0f );
-	//glVertex2f( 0, height );
-
-	/*
-	 *
-	 */
-
-	//glTexCoord2f(-0.5, -0.5); 
-	//glVertex3f(-1, -1, -0.5f);
-
-	//glTexCoord2f(2*width - 0.5, 0);
-	//glVertex3f( 1, -1, -0.5f);
-
-	//glTexCoord2f(2*width - 0.5, 2*height - 0.5);
-	//glVertex3f( 1,  1, -0.5f);
-
-	//glTexCoord2f(0, 2*height - 0.5);
-	//glVertex3f(-1,  1, -0.5f);
-
 
 	glVertex2f(0.0, 0.0);
 	glVertex2f(width, 0.0);
@@ -246,4 +227,5 @@ FloatTextProcessor::~FloatTextProcessor(void)
 	delete iFbo;
 	delete iFragmentShader;
 	delete iShaderProgram;
+	glDeleteTextures(2, iTextures);
 }
