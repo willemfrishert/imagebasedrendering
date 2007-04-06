c68bd906a5558b48879d2a326bdae88427a39293
wfrishert <wfrishert@15324175-3028-0410-9899-2d1205849c9d>
2007-04-06 09:19:32 +0000

    backup before adding multiple blur shaders for each mipmap level

--- trunk/Project/Code/inc/BloomEffect.h
+++ trunk/Project/Code/inc/BloomEffect.h
@@ -6,7 +6,8 @@
 #include "ShaderObject.h"
 #include "ShaderUniformValue.h"
 
-const int KNumberOfBlurMaps = 4;
+const int KNumberOfBlurLevels = 4;
+
 class BloomEffect
 {
 public:
@@ -16,33 +17,72 @@ public:
 	void End();
 
 private:
+
+	enum TTextureSize
+	{
+		ETextureSize512 = 512,
+		ETextureSize256 = 256,
+		ETextureSize128 = 128,
+		ETextureSize64  = 64,
+		ETextureSize32  = 32,
+		ETextureSize16  = 16
+	};
+
+	enum TMipMapLevel
+	{
+		EMipMapLevel512 = 0,
+		EMipMapLevel256,
+		EMipMapLevel128,
+		EMipMapLevel64,
+		EMipMapLevel32,
+		EMipMapLevel16,
+		EMipMapLevel8,
+		EMipMapLevel4,
+		EMipMapLevel2,
+		EMipMapLevel1
+	};
+
+	enum TTextureID
+	{
+		ETextureId128 = 0,
+		ETextureId64,
+		ETextureId32,
+		ETextureId16
+	};
+
+
 	void Init();
 	void InitTextures();
 	void InitFramebufferObject();
 	void InitShaders();
 
-	void BlurMipmap( GLuint aTextureID,  GLuint aMipmapSize,  GLfloat aMipmapLevel, GLfloat aUpscaleFactor, FrameBufferObject* aFinalFBO );
+	void BlurMipmap( GLuint aTextureID, GLuint aIntermediateTextureNumber, GLuint aMipmapSize, GLuint aMipmapLevel, FrameBufferObject* aFinalFBO );
 
 	void RenderSceneOnQuad( GLuint aTextureID);
 	void RenderSceneOnQuad(  GLuint aOriginalTexture, GLuint* aUpscaledTexture );
 	void UpscaleTexture( GLuint aTextureID, GLfloat aTextureCoord );
 	void RenderBloomEffect(GLuint aOriginalTexture, GLuint* aUpscaledTexture);
 
-	FrameBufferObject* iOriginalFBO;
-	FrameBufferObject* iIntermediateFBO;
-	FrameBufferObject* iFinalBlurFBO;
+
 
 	GLint iOldViewPort[4];
 
-	GLuint iImageWinWidth;
-	GLuint iImageWinHeight;
+	GLuint iOriginalImageSize;
 
-	GLuint iImageBlurWidth;
-	GLuint iImageBlurHeight;
+	//GLuint iImageBlurWidth;
+	//GLuint iImageBlurHeight;
+
+
+	FrameBufferObject* iOriginalFBO;
+	FrameBufferObject* iIntermediateFBO[KNumberOfBlurLevels];
+	FrameBufferObject* iFinalBlurFBO;
 
 	GLuint iOriginalTexture;
-	GLuint iHorizBlurredTexture, iFinalBlurredTexture;
-	GLuint iUpscaledTexture[KNumberOfBlurMaps];
+	GLuint iHorizBlurredTexture[KNumberOfBlurLevels];
+	GLuint iFinalBlurredTexture[KNumberOfBlurLevels];
+	GLuint iUpscaledTexture[KNumberOfBlurLevels];
+
+	GLuint iMipmapSize[KNumberOfBlurLevels];
 
 
 	// ########### SHADERS DECLARATIONS ###########
@@ -50,19 +90,20 @@ private:
 	ShaderProgram* iVerticalShaderProgram;
 	ShaderProgram* iBlenderShaderProgram;
 
-	ShaderObject* iBlenderVertexShader;
 	ShaderObject* iBlenderFragmentShader;
-
-	ShaderObject* iHorizontalBlurVertexShader;
 	ShaderObject* iHorizontalBlurFragmentShader;
-
-	ShaderObject* iVerticalBlurVertexShader;
 	ShaderObject* iVerticalBlurFragmentShader;
 
 	ShaderUniformValue<int> iTextureOriginalUniform;
 	ShaderUniformValue<int> iTextureHorizontalUniform;
 	ShaderUniformValue<int> iMipmapSizeUniform;
 	ShaderUniformValue<int> iHorizTextureSizeUniform;
+	ShaderUniformValue<float> iHorizBlurWeight1Uniform;
+	ShaderUniformValue<float> iHorizBlurWeight2Uniform;
+	ShaderUniformValue<float> iHorizBlurWeight3Uniform;
+	ShaderUniformValue<float> iVertBlurWeight1Uniform;
+	ShaderUniformValue<float> iVertBlurWeight2Uniform;
+	ShaderUniformValue<float> iVertBlurWeight3Uniform;
 	
 	ShaderUniformValue<int> iBlenderOriginalTextureUniform;
 	ShaderUniformValue<int> iBlenderBlur1TextureUniform;
@@ -72,5 +113,4 @@ private:
 
 	ShaderUniformValue<float> iBlurDeltaUniform;
 	ShaderUniformValue<float> iMipmapLevelUniform;
-
 };
