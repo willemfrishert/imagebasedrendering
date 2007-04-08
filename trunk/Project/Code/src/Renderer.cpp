49bfd7d23ed30661a9b53c609d1e0c926143deba
jpjorge <jpjorge@15324175-3028-0410-9899-2d1205849c9d>
2007-04-08 13:10:51 +0000

    IBLPerfectReflection; VirtualTrackball; PhotographicToneMapper; CubeMap; ScreenCapture

--- trunk/Project/Code/src/Renderer.cpp
+++ trunk/Project/Code/src/Renderer.cpp
@@ -1,6 +1,18 @@
 //INCLUDES
+#include "CubeMap.h"
+#include "Vector3.h"
+#include "Matrix4.h"
+#include "Quaternion.h"
+#include "VirtualTrackball.h"
 #include "Renderer.h"
 #include "Vertex.h"
+#include "FrameBufferObject.h"
+#include "ScreenCapture.h"
+#include "LuminanceConverter.h"
+#include "GPUParallelReductor.h"
+#include "IBLPerfectReflection.h"
+#include "PhotographicToneMapper.h"
+
 
 //USE THIS FILE
 
@@ -20,6 +32,7 @@ const float KZFar	= 600.0f;
 //INIT STATIC DATA
 Renderer* Renderer::iCurrentRenderer = 0;
 
+
 //CONSTRUCTORS
 //
 //Default constructor
@@ -33,10 +46,11 @@ Renderer::Renderer()
 , iOldYRotation( 0 )
 , iXPosition( 0 )
 , iYPosition( 0 )
-, iZoom( -25 )
+, iZoom( -1 )
 {
 	InitMain();
 	Renderer::iCurrentRenderer = this;
+	iCubeMap = new CubeMap( 4 );
 }
 
 
@@ -45,6 +59,11 @@ Renderer::Renderer()
 //
 Renderer::~Renderer()
 {
+	delete iScreenCapture;
+	delete iLuminanceConverter;
+	delete iCubeMap;
+	delete iToneMapper;
+	delete iIBLReflection;
 }
 
 void Renderer::InitShaders()
@@ -75,8 +94,24 @@ void Renderer::InitMain()
  */
 void Renderer::CreateScene()
 {
-}
+	unsigned char minExponent;
+	unsigned char maxExponent;
+	iCubeMap->setupCompressedCubeMap("./textures/stpeters", minExponent, maxExponent);
+	//iCubeMap->setupCubeMap("./textures/rnl");
+	//iCubeMap->setupCubeMap("./textures/stpeters");
 
+	/************************************************************************/
+	/* XXX:                                                                 */
+	/************************************************************************/
+	this->iLuminanceConverter = new LuminanceConverter(KTextureWidth, KTextureHeight, -1, -1);
+	
+	// Setup Scene Texture
+	this->iScreenCapture = new ScreenCapture(KTextureHeight, KTextureWidth);
+
+	this->iToneMapper = new PhotographicToneMapper(0, 0, KTextureWidth, KTextureHeight);
+
+	this->iIBLReflection = new IBLPerfectReflection("./shader/iblreflection.vert", "./textures/stpeters_panorama1024.hdr");
+}
 
 /** \brief Method that calculates iFrames per second
 *
@@ -104,6 +139,27 @@ void Renderer::FramesPerSec()
 		iFrame = 0;
 	}
 
+}
+
+/**
+ * @param aValue
+ */
+void Renderer::SetExposure(float aValue)
+{
+	this->iToneMapper->setExposure(aValue);
+}
+
+/**
+ * @return 
+ */
+float Renderer::GetExposure()
+{
+	return this->iToneMapper->getExposure();
+}
+
+void Renderer::MouseMoved()
+{
+	iToneMapper->InvalidateExposure();
 }
 
 /** \brief Method that draws text to the glut window
@@ -164,10 +220,57 @@ void Renderer::InitLights()
  */
 void Renderer::RenderScene()
 {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	static Matrix4 trackballMatrix;
+	static float trackballMatrixGL[16];
+
+	// Setup FBO to render the scene to a texture
+	iScreenCapture->startCapture();
 	
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
+
+	glPushMatrix();
+	{
+		glTranslatef(0, 0, iZoom);
+
+		/************************************************************************/
+		/* OPTMIZE PLEASE!!!                                                    */
+		/************************************************************************/
+
+		trackballMatrix = trackball.getRotationMatrix( );
+		trackballMatrix.getGLMatrix( trackballMatrixGL );
+		glMultMatrixf( trackballMatrixGL );
+
+		float inverseGL[16];
+		Matrix4 inverse = Matrix4::inverse( trackballMatrix );
+		inverse.getGLMatrix( inverseGL );
+
+		//iCubeMap->render( inverseGL );
+
+		iIBLReflection->start();
+		{
+			glutSolidSphere(0.25, 32, 32);
+		}
+		iIBLReflection->stop();
+	}
+	glPopMatrix();
+
+	// unbind and render
+	GLuint capturedSceneTexId = iScreenCapture->stopCapture();
+
+	//GLuint finalScene = iLuminanceConverter->processData(capturedSceneTexId);
+
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	//iToneMapper->toneMap(capturedSceneTexId, finalScene);
+
+	RenderSceneOnQuad(capturedSceneTexId, GL_TEXTURE_2D, 512, 512);
+
+	//iScreenCapture->renderToScreen();
 
 	// Draw Text
 	FramesPerSec();
@@ -175,6 +278,63 @@ void Renderer::RenderScene()
 	DrawText();
 	
 	glutSwapBuffers();
+}
+
+void Renderer::RenderSceneOnQuad(GLuint textureId, GLenum target, 
+								 int width, int height)
+{
+	glEnable( target );
+	
+	glMatrixMode( GL_PROJECTION );
+	glPushMatrix();
+	{
+		glLoadIdentity();
+		gluOrtho2D( 0.0f, 1.0f, 0.0f, 1.0f);
+		//gluOrtho2D( 0.0f, width, 0.0f, height);
+
+		glMatrixMode( GL_MODELVIEW );
+		glPushMatrix();
+		{
+			glLoadIdentity();
+
+			glBindTexture(target, textureId);
+			glBegin(GL_QUADS);
+			{
+				glTexCoord2i(0, 0);
+				glVertex2f(0.0f, 0.0f);
+				
+				glTexCoord2i(1, 0);
+				glVertex2f(1.0f, 0.0f);
+				
+				glTexCoord2i(1, 1);
+				glVertex2f(1.0f, 1.0f);
+				
+				glTexCoord2i(0, 1);
+				glVertex2f(0.0f, 1.0f);
+
+				//glTexCoord2i(0, 0);
+				//glVertex2f(0.0, 0.0);
+
+				//glTexCoord2i(width, 0);
+				//glVertex2f(width, 0.0);
+
+				//glTexCoord2i(width, height);
+				//glVertex2f(width, height);
+
+				//glTexCoord2i(0.0, height);
+				//glVertex2f(0.0, height);
+
+			}
+			glEnd();
+		}
+		glPopMatrix();
+
+		glMatrixMode( GL_PROJECTION );
+	}
+	glPopMatrix();
+	glMatrixMode( GL_MODELVIEW );
+	
+	glDisable( target );
 }
 
 void Renderer::SetXRotation(float aXRotation)
@@ -255,6 +415,11 @@ float Renderer::GetScreenHeightInPixels()
 	return this->iScreenHeight;
 }
 
+VirtualTrackball& Renderer::GetTrackball()
+{
+	return this->trackball;
+}
+
 //-----------------------------
 //resize the window
 //-----------------------------
@@ -265,7 +430,7 @@ void Renderer::ResizeScene(const int aWidth, const int aHeight)
 	// Projection transformation
 	glMatrixMode(GL_PROJECTION); // Specifies which matrix stack is the target for matrix operations
 	glLoadIdentity(); // We initialize the projection matrix as identity
-	gluPerspective(60.0, (GLfloat)aWidth/(GLfloat)aHeight, 1.5, 500.0);
+	gluPerspective(60.0, (GLfloat)aWidth/(GLfloat)aHeight, 0.1, 500.0);
 
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
@@ -273,6 +438,8 @@ void Renderer::ResizeScene(const int aWidth, const int aHeight)
 	this->iScreenHeight = aHeight;
 	// This command redraw the scene (it calls the same routine of glutDisplayFunc)
 	//glutPostRedisplay ();
+
+	this->trackball.SetWindowRect(aWidth, aHeight);
 }
 
 //EXTERNAL FUNCTIONS TO USE GLUT CALLBACKS
