82c1aab2ea529038ed861680db76e4a638802fb8
wfrishert <wfrishert@15324175-3028-0410-9899-2d1205849c9d>
2007-04-12 09:43:42 +0000

    Tried some quickfixes. Time to clean up the code

--- trunk/Project/Code/inc/BloomEffect.h
+++ trunk/Project/Code/inc/BloomEffect.h
@@ -9,9 +9,6 @@
 #include "GPUParallelReductor.h"
 #include "BlurTexture.h"
 
-//const int KNumberOfBlurLevels = 4;
-//const int KNumberOfBlurShaders = 1;
-
 class BloomEffect
 {
 public:
@@ -22,54 +19,20 @@ public:
 
 private:
 
-	enum TTextureSize
-	{
-		ETextureSize512 = 512,
-		ETextureSize256 = 256,
-		ETextureSize128 = 128,
-		ETextureSize64  = 64,
-		ETextureSize32  = 32,
-		ETextureSize16  = 16
-	};
-
-	enum TMipMapLevel
-	{
-		EMipMapLevel512 = 0,
-		EMipMapLevel256,
-		EMipMapLevel128,
-		EMipMapLevel64,
-		EMipMapLevel32,
-		EMipMapLevel16,
-		EMipMapLevel8,
-		EMipMapLevel4,
-		EMipMapLevel2,
-		EMipMapLevel1
-	};
-
-	enum TTextureID
-	{
-		ETextureId128 = 0,
-		ETextureId64,
-		ETextureId32,
-		ETextureId16
-	};
-
-
 	void Init();
 	void InitTextures();
 	void InitFramebufferObject();
 	void InitShaders();
 	void InitCodecShaderObject();
 	void InitBrightpassShader();
-	//void InitBlurShaders();
 	void InitBlendShader();
 
-	//void BlurMipmap( GLuint aTextureID, GLuint aCounter, GLuint aMipmapSize, GLuint aMipmapLevel );
-
 	void RenderSceneOnQuad( GLuint aTextureID);
-	//void RenderSceneOnQuad( GLuint aTextureID, GLuint aVertexSize );
 	void RenderSceneOnQuad(  GLuint aOriginalTexture, GLuint* aBlurredTextures );
 	void RenderBloomEffect(GLuint aOriginalTexture, GLuint* aBlurredTextures);
+	void EnableMultitexturing( GLuint aOriginalTexture,  GLuint aLuminanceTexture );
+	void DisableMultitexturing();
+	void RenderSceneOnQuadMultiTex(GLuint aOriginalTexture, GLuint aLuminanceTexture);
 
 	GLint iOldViewPort[4];
 
@@ -77,56 +40,36 @@ private:
 
 
 	FrameBufferObject* iOriginalFBO;
-	//FrameBufferObject* iIntermediateFBO[KNumberOfBlurLevels];
 	FrameBufferObject* iBlendedFBO;
 
 	GLuint iOriginalTexture;
-	GLuint iBrightpassTexture;
-	//GLuint iHorizBlurredTexture[KNumberOfBlurLevels];
-	//GLuint iFinalBlurredTexture[KNumberOfBlurLevels];
+	GLuint iBrightpassTexture[2];
 	GLuint iBlendedTexture;
 
-	//GLuint iMipmapSize[KNumberOfBlurLevels];
-	//GLuint iMipmapDelta[KNumberOfBlurLevels];
+	unsigned int iBrightpassToggle;
+	GLenum iBrightpassFBOAttachment[2];
 
 	LuminanceConverter* iLuminanceConverter;
 	GPUParallelReductor* iGPUParallelReductor;
 	float logSum[4];
 
 	BlurTexture* iBlurTexture;
-	GLuint* finalBlurredTextures;
+	GLuint* iFinalBlurredTexture;
 
 	// ########### SHADERS DECLARATIONS ###########
 	ShaderProgram* iBrightpassShaderProgram;
-	//ShaderProgram* iHorizontalShaderProgram;
-	//ShaderProgram* iVerticalShaderProgram;
 	ShaderProgram* iBlenderShaderProgram;
 
 	ShaderObject* iBrightpassFragmentShader;
-	//ShaderObject* iHorizontalBlurFragmentShader;
-	////ShaderObject* iHorizontalBlurVertexShader;
-	//ShaderObject* iVerticalBlurFragmentShader;
 	ShaderObject* iBlenderFragmentShader;
 	ShaderObject* iCodecRGBEFragmentShader;
 
 	ShaderUniformValue<float> iLogAvgLum;
-
-	//ShaderUniformValue<int> iTextureOriginalUniform;
-	//ShaderUniformValue<int> iTextureHorizontalUniform;
-	//ShaderUniformValue<int> iMipmapSizeUniform;
-	//ShaderUniformValue<int> iHorizTextureSizeUniform;
-	//ShaderUniformValue<float> iMipmapLevelUniform;
-
-	//ShaderUniformValue<float> iHorizBlurWeight1Uniform;
-	//ShaderUniformValue<float> iHorizBlurWeight2Uniform;
-	//ShaderUniformValue<float> iHorizBlurWeight3Uniform;
-	//ShaderUniformValue<float> iVertBlurWeight1Uniform;
-	//ShaderUniformValue<float> iVertBlurWeight2Uniform;
-	//ShaderUniformValue<float> iVertBlurWeight3Uniform;
 
 	ShaderUniformValue<int> iBlenderOriginalTextureUniform;
 	ShaderUniformValue<int> iBlenderBlur1TextureUniform;
 	ShaderUniformValue<int> iBlenderBlur2TextureUniform;
 	ShaderUniformValue<int> iBlenderBlur3TextureUniform;
 	ShaderUniformValue<int> iBlenderBlur4TextureUniform;
+	ShaderUniformValue<int> iPrevBrightpassTextureUniform;
 };
