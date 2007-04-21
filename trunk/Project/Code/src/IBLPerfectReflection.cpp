2e3770bd898d558da5a0b34c66e9c49f58944d78
jpjorge <jpjorge@15324175-3028-0410-9899-2d1205849c9d>
2007-04-21 09:45:09 +0000

    using cube map now

--- trunk/Project/Code/src/IBLPerfectReflection.cpp
+++ trunk/Project/Code/src/IBLPerfectReflection.cpp
@@ -10,7 +10,7 @@
 
 IBLPerfectReflection::IBLPerfectReflection(const string& ashaderFilename, const string& aPanoramaFilename)
 {
-	setupIrradianceMapTexture( aPanoramaFilename );
+	//setupIrradianceMapTexture( aPanoramaFilename );
 
 	initShaders( ashaderFilename );
 }
@@ -22,10 +22,10 @@ IBLPerfectReflection::~IBLPerfectReflection(void)
 /**
  *
  */
-void IBLPerfectReflection::start()
+void IBLPerfectReflection::start(GLuint aCubeMapTexId)
 {
-	glEnable(GL_TEXTURE_2D);
-	glBindTexture(GL_TEXTURE_2D, iIrradianceMapTexId);
+	glEnable(GL_TEXTURE_CUBE_MAP);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, aCubeMapTexId);
 
 	iShaderProgram->useProgram();
 }
@@ -36,36 +36,8 @@ void IBLPerfectReflection::start()
 void IBLPerfectReflection::stop()
 {
 	iShaderProgram->disableProgram();
-}
 
-/**
- *
- */
-void IBLPerfectReflection::setupIrradianceMapTexture(const string& aPanoramaFilename)
-{
-	HDRLoaderResultEncoded hdrPic;
-	HDRLoader::load(aPanoramaFilename.c_str(), hdrPic);
-
-	glEnable(GL_TEXTURE_2D);
-
-	glGenTextures(1, &iIrradianceMapTexId);
-
-	// NOTE: use GL_TEXTURE_RECTANGLE_ARB in order to specify coords
-	// from [0, height] x [0, width]
-	glBindTexture(GL_TEXTURE_2D, iIrradianceMapTexId);
-
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-	// NOTE: as opposed to what they say in the extension description of 
-	// GL_TEXTURE_RECTANGLE_ARB, ATI seems to not support 
-	// the GL_REPEAT for wrapping modes
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
-
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16, hdrPic.width, 
-		hdrPic.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, hdrPic.cols);
+	glDisable(GL_TEXTURE_CUBE_MAP);
 }
 
 /**
@@ -74,19 +46,19 @@ void IBLPerfectReflection::setupIrradianceMapTexture(const string& aPanoramaFile
 void IBLPerfectReflection::initShaders(const string& ashaderFilename)
 {
 	iShaderProgram			= new ShaderProgram();
-	iIrradianceMapUniform	= new ShaderUniformValue<int>();
+	iCubeMapUniform	= new ShaderUniformValue<int>();
 
 	iFragmentShader			= new ShaderObject(GL_FRAGMENT_SHADER, ashaderFilename + ".frag");
 	iVertexShader			= new ShaderObject(GL_VERTEX_SHADER, ashaderFilename + ".vert");
 
 	// Uniforms
-	iIrradianceMapUniform->setValue( 0 );
-	iIrradianceMapUniform->setName("irradianceMap");
+	iCubeMapUniform->setValue( 0 );
+	iCubeMapUniform->setName("reflectionCubeMap");
 
 	iShaderProgram->attachShader( *iFragmentShader );
 	iShaderProgram->attachShader( *iVertexShader );
 	iShaderProgram->attachShader( *CodecRGBE::getShaderObject() );
-	iShaderProgram->addUniformObject( iIrradianceMapUniform );
+	iShaderProgram->addUniformObject( iCubeMapUniform );
 
 	// after all the shaders have been attached
 	iShaderProgram->buildProgram();
