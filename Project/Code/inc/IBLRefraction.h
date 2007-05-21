37941f40cdb3bc3daa51a7951107a948a010a6d0
jpjorge <jpjorge@15324175-3028-0410-9899-2d1205849c9d>
2007-05-21 21:31:59 +0000

    support for key pressing to change interface refraction coefficient

--- Project/Code/inc/IBLRefraction.h
+++ Project/Code/inc/IBLRefraction.h
@@ -6,8 +6,9 @@ class ShaderObject;
 class ShaderProgram;
 template <class T>
 class ShaderUniformValue;
+class IKeyListener;
 
-class IBLRefraction : public Material
+class IBLRefraction : public Material, public IKeyListener
 {
 public:
 	IBLRefraction(float aEta1, float aEta2, const string& ashaderFilename, GLuint aCubeMapTexId);
@@ -19,11 +20,17 @@ public:
 
 	void setEnvironmentCubeMap( GLuint aCubeMapTexId );
 
+	virtual void ProcessNormalKeys( unsigned char key, int x, int y );
+	
+	virtual void ProcessCursorKeys( int key, int x, int y );
+
 
 	// methods
 private:
 
 	void initShaders(const string& ashaderFilename);
+
+	void computeEta(float& eta, float& f);
 
 	// attributes
 private:
